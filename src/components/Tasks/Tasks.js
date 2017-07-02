2552b487213a07687b9fac5e6354684604d9e37e
Michael Minutelli <mminutel@gmail.com>
2017-07-02 10:57:07 -0400

    Added the structure for the task component.

---
+++ src/components/Tasks/Tasks.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Tasks.css';
+
+class Tasks extends Component {
+
+  render() {
+    return (
+      <div>
+        Tasks
+      </div>
+    );
+  }
+
+}
+
+export default Tasks;
