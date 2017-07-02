7ea919cdf96635643513db5b10e5bf4e544066ce
Michael Minutelli <mminutel@gmail.com>
2017-07-02 11:35:19 -0400

    Created views, etc. for task component.

--- src/components/Tasks/Tasks.js
+++ src/components/Tasks/Tasks.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './Tasks.css';
 
 class Tasks extends Component {
@@ -6,11 +7,39 @@ class Tasks extends Component {
   render() {
     return (
       <div>
-        Tasks
+        <Switch>
+          <Route exact path='/tasks' component={TasksViewAll} />
+          <Route exact path='/tasks/create' component={TasksCreate} />
+          <Route path='/tasks/:taskId' component={TasksView} />
+        </Switch>
       </div>
     );
   }
 
+}
+
+const TasksViewAll = (props) => {
+  return (
+    <div>
+      All Tasks
+    </div>
+  )
+}
+
+const TasksCreate = (props) => {
+  return (
+    <div>
+      Create Task
+    </div>
+  )
+}
+
+const TasksView = (props) => {
+  return (
+    <div>
+      View Task
+    </div>
+  )
 }
 
 export default Tasks;
