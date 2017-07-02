70368d73fd8d22279fd237043efc778c1dee5aa1
Michael Minutelli <mminutel@gmail.com>
2017-07-02 01:58:50 -0400

    Refactored the project structure.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 import './index.css';
-import App from './containers/App';
+import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
